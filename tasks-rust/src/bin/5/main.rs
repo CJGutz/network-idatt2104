50ebbc59fdb5b937d64acc279359317b3c111c90
Carl Gützkow <cjgutzkow@gmail.com>
2023-02-13 21:33:43 +0100

    create server which uses docker to execute rust code

--- tasks-rust/src/bin/5/main.rs
+++ tasks-rust/src/bin/5/main.rs
@@ -1,12 +1,15 @@
 use std::{
+    fs,
     io::{Read, Write},
     net::{Shutdown, TcpListener, TcpStream},
     ops::Add,
+    process::Command,
 };
+use urlencoding::decode;
 
 use tasks_rust::workers::Workers;
 
-const ADDRESS: &str = "0.0.0.0:8080";
+const ADDRESS: &str = "127.0.0.1:8080";
 const NUMBER_OF_THREADS: u32 = 4;
 
 fn main() {
@@ -37,40 +40,69 @@ fn main() {
 }
 
 fn handle_tcp_stream(stream: &mut TcpStream) {
-    stream.set_read_timeout(None).unwrap();
-
     let buf = &mut [0; 200];
     match stream.read(buf) {
         Ok(_) => (),
         Err(_) => println!("Connection lost"),
     };
-    let request_string = buf
-        .iter()
-        .map(|&x| x as char)
-        .collect::<String>()
-        .trim()
-        .to_string();
+    let request_string = buf.iter().map(|&x| x as char).collect::<String>();
 
-    println!("Request:\n{}", request_string);
+    let code = parse_url(&request_string);
 
-    let mut headers: Vec<&str> = request_string.split("\n").collect();
-    headers.remove(0);
+    let result = match code {
+        Ok(code) => run_code(code.as_str()),
+        Err(e) => String::from(format!("There was an error parsing your request {:?}", e)),
+    };
 
-    let mut response = String::from("HTTP/1.1 200 OK\nContent-Type: text/html; charset=utf-8\n\n");
-    response =
-        response.add(
-            format!(
-            "<html><body><h1>Welcome to this site!</h1><p>Headers:</p><ul>{}</ul></body></html>",
-            headers.iter().map(|header| format!("<li>{}</li>", header)).collect::<String>()
-        )
-            .as_str(),
-        );
+    let index_file = fs::read_to_string("./src/bin/5/index.html")
+        .expect("Could not read html file")
+        .add(format!("<pre>{}</pre></body></html>", result).as_str());
+    let response = format!(
+        "HTTP/1.1 200 OK\nContent-Type: text/html; charset=utf-8\n\n{}",
+        index_file
+    );
 
-    // Send response
     stream
         .write(response.as_bytes())
         .expect("Could not write to stream.");
 
     stream.flush().unwrap();
-    stream.shutdown(Shutdown::Both).unwrap();
+    stream.shutdown(Shutdown::Both).unwrap_or_default();
+}
+
+fn parse_url(url: &str) -> Result<String, String> {
+    let start = match url.find("code=") {
+        Some(size) => size,
+        None => return Err("Request did not contain the code attribute".to_string()),
+    } + 5;
+    let end = url
+        .find(" HTTP/")
+        .unwrap_or(url.find("\n").unwrap_or(url.len()));
+    let code = url[start..end].to_string();
+    let decoded = decode(code.as_str())
+        .expect("Code could not be decoded")
+        .into_owned()
+        .replace("+", " ");
+    Ok(decoded)
+}
+
+fn run_code(code: &str) -> String {
+    let command = format!("printf {:?} > main.rs && rustc main.rs && ./main", code);
+    let result = Command::new("docker")
+        .arg("run")
+        .arg("--rm")
+        .arg("rust:alpine3.17")
+        .arg("sh")
+        .arg("-c")
+        .arg(command)
+        .stdout(std::process::Stdio::piped())
+        .output()
+        .expect("Could not run docker");
+
+    let error = result.stderr.iter().map(|&x| x as char).collect::<String>();
+    let output = result.stdout.iter().map(|&x| x as char).collect::<String>();
+    if error.len() > 0 {
+        return error;
+    }
+    output
 }
